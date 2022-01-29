3f556cf7f982bb0ce40a8d24000253e454536d2b
Vaishnavi <vaishnavisahu16@gmail.com>
2022-01-30 02:18:21 +0530

    Add gitignore

--- app.js
+++ app.js
@@ -124,8 +124,11 @@ app.post("/work", function(req, res){
   res.redirect("/work");
 });
 
+let port = process.env.PORT;
+if(port == null || port == ""){
+  port = 3000;
+}
 
-
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Server started at port 3000");
+app.listen( port, function(){
+  console.log("Server has started Successfully");
 });
